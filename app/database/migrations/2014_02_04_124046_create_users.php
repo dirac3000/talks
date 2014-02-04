6290bcca279d189422296edd345fe07dc5450d9a
Alvaro Moran <alvaro.moran@gmail.com>
2014-02-05 00:15:19 +0100

    added manager to users table

--- app/database/migrations/2014_02_04_124046_create_users.php
+++ app/database/migrations/2014_02_04_124046_create_users.php
@@ -19,14 +19,39 @@ class CreateUsers extends Migration {
 			$table->string('email', 64)->nullable(); /* we won't use this if we use LDAP */
 			$table->enum('rights', 
 				array('admin', 'advanced', 'simple'))->default('simple');
+			$table->integer('manager_id')->unsigned()->nullable();
+			$table->foreign('manager_id')->references('id')->on('users');
 		});
 
 		DB::table('users')->insert(array(
             'username'  => 'admin',
             'password'  => Hash::make('password'),
-            'rights'  => 'admin'
+            'rights'  => 'admin',
         ));
-	}
+
+		DB::table('users')->insert(array(
+            'username'  => 'henri',
+            'password'  => Hash::make('password'),
+            'email' => 'henri@example.com',
+            'rights'  => 'advanced',
+        ));
+
+		DB::table('users')->insert(array(
+            'username'  => 'christopher',
+            'password'  => Hash::make('password'),
+            'email' => 'christopher@example.com',
+            'rights'  => 'advanced',
+            'manager_id' => 2, /* I know henri will be 2 */
+        ));
+
+		DB::table('users')->insert(array(
+            'username'  => 'karine',
+            'password'  => Hash::make('password'),
+            'email' => 'karine@example.com',
+            'rights'  => 'simple',
+            'manager_id' => 3, /* I know christophe will be 3 */ 
+        ));
+}
 
 	/**
 	 * Reverse the migrations.
