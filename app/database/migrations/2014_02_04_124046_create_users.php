bb272531c37853cc3ec22c617788a8a9eb3e0576
Alvaro Moran <alvaro.moran@gmail.com>
2014-02-05 23:23:55 +0100

    users seed take off table migration

--- app/database/migrations/2014_02_04_124046_create_users.php
+++ app/database/migrations/2014_02_04_124046_create_users.php
@@ -22,36 +22,7 @@ class CreateUsers extends Migration {
 			$table->integer('manager_id')->unsigned()->nullable();
 			$table->foreign('manager_id')->references('id')->on('users');
 		});
-
-		DB::table('users')->insert(array(
-            'username'  => 'admin',
-            'password'  => Hash::make('password'),
-            'rights'  => 'admin',
-        ));
-
-		DB::table('users')->insert(array(
-            'username'  => 'henri',
-            'password'  => Hash::make('password'),
-            'email' => 'henri@example.com',
-            'rights'  => 'advanced',
-        ));
-
-		DB::table('users')->insert(array(
-            'username'  => 'christopher',
-            'password'  => Hash::make('password'),
-            'email' => 'christopher@example.com',
-            'rights'  => 'advanced',
-            'manager_id' => 2, /* I know henri will be 2 */
-        ));
-
-		DB::table('users')->insert(array(
-            'username'  => 'karine',
-            'password'  => Hash::make('password'),
-            'email' => 'karine@example.com',
-            'rights'  => 'simple',
-            'manager_id' => 3, /* I know christophe will be 3 */ 
-        ));
-}
+	}
 
 	/**
 	 * Reverse the migrations.
--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,35 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		// $this->call('UserTableSeeder');
+		DB::table('users')->insert(array(
+            'username'  => 'admin',
+            'password'  => Hash::make('password'),
+            'rights'  => 'admin',
+        ));
+
+		DB::table('users')->insert(array(
+            'username'  => 'henri',
+            'password'  => Hash::make('password'),
+            'email' => 'henri@example.com',
+            'rights'  => 'advanced',
+        ));
+
+		DB::table('users')->insert(array(
+            'username'  => 'christopher',
+            'password'  => Hash::make('password'),
+            'email' => 'christopher@example.com',
+            'rights'  => 'advanced',
+            'manager_id' => 2, /* I know henri will be 2 */
+        ));
+
+		DB::table('users')->insert(array(
+            'username'  => 'karine',
+            'password'  => Hash::make('password'),
+            'email' => 'karine@example.com',
+            'rights'  => 'simple',
+            'manager_id' => 3, /* I know christophe will be 3 */ 
+        ));	
+
 	}
 
 }
